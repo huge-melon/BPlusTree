0a8ee84aad895fd7c198d422cc644ebd389022ed
huge-melon <shixin1725@gmail.com>
2019-11-20 10:50:22 +0800

    third

--- run.sh
+++ run.sh
@@ -13,38 +13,39 @@ then
     rm -r err_info
 fi
 
-count=1500 # 12小时测试，日志每30s记录一次，因此循环1500次
+count=4500 # 12小时测试，日志每10s记录一次，因此循环4500次
 
 ./test >>log 2>>err_info & # 运行B+树并发读写，将错误信息保存到err_info中
-sleep 2
+# ./test & 
+sleep 1
 
 while [ $count -ge 0 ]
 do
-let count--
+    let count--
 
-cpu_mess=`pidstat -uI -G "test"|sed -n '4p'|awk '{print $1,$3,$7}'`
-mem_mess=`pidstat -r -G "test"|sed -n '4p'|awk '{print $7,$8}'`
+    cpu_mess=`pidstat -uI -G "test"|sed -n '4p'|awk '{print $1,$3,$7}'`
+    mem_mess=`pidstat -r -G "test"|sed -n '4p'|awk '{print $7,$8}'`
 
-# cpu[0] 获取时间信息 cpu[1]获得pid cup[2]获取CPU的平均占用率 
-i=0
-for element in $cpu_mess
-do 
-cpu[i]=$element
-let i++
-done
+    # cpu[0] 获取时间信息 cpu[1]获得pid cup[2]获取CPU的平均占用率 
+    i=0
+    for element in $cpu_mess
+    do 
+        cpu[i]=$element
+        let i++
+    done
 
-# mem[0] 已占用内存大小KB mem[1]内存占用率
-i=0
-for element in $mem_mess
-do
-    mem[i]=$element
-    let i++
-done
+    # mem[0] 已占用内存大小KB mem[1]内存占用率
+    i=0
+    for element in $mem_mess
+    do
+        mem[i]=$element
+        let i++
+    done
 
-output_mess=${cpu[0]}' 进程id:'${cpu[1]}'  CPU占用率:'${cpu[2]}'%  已占用内存:'${mem[0]}'KB  内存占用率:'${mem[1]}'%'
-echo $output_mess>>log # 将系统资源信息输出到log文件中
+    output_mess=${cpu[0]}' 进程id:'${cpu[1]}'  CPU占用率:'${cpu[2]}'%  已占用内存:'${mem[0]}'KB  内存占用率:'${mem[1]}'%'
+    echo $output_mess>>log # 将系统资源信息输出到log文件中
 
-sleep 10
+    sleep 10 # 10s
 done
 
 kill ${cpu[1]}
--- test.cpp
+++ test.cpp
@@ -5,64 +5,79 @@
 #include "b_plus_tree.h"
 
 using namespace std;
-
+#define BUF_SIZE 100
+#define ORDER 8
 int main()
 {
-	int order = 8;
-	int buf_size =50;
 	srand((unsigned int)time(NULL));
-	BPlusTree bpt(order,buf_size);
-	cout << "B+树初始化：序数 "<<order<<" 任务缓冲区大小 "<<buf_size<<endl;
+	BPlusTree bpt(ORDER,BUF_SIZE);
+	cout << "B+树初始化：序数 "<<ORDER<<" 任务缓冲区大小 "<<BUF_SIZE<<endl;
 	int data_size = 1e8;
 	cout << "开始插入1亿条数据"<<endl;
 	for(int i=0;i<data_size;i++){
-		bpt.insert_data(rand(), rand());
+		bpt.insert_data(rand()/10, rand()/10);
 	}
 	cout <<"1亿条数据插入完成"<<endl << "开始多线程执行："<<endl;
 	
-	std::thread productor_threads[buf_size*2];
-	std::thread consumer_threads[buf_size*2];
+	std::thread productor_threads[BUF_SIZE];
+	std::thread consumer_threads[BUF_SIZE];
+	srand((unsigned int)time(NULL));
 
 	while (1) {
-		int productor_num = rand()%(buf_size); // 随机生成生产者的数量，其大小在缓冲区大小以内
-		for(int i=0;i<productor_num;i++){ // 各个操作之间的比例   插入：删除：查找：更新 = 4:4:1:1
+
+		// for(int i=0;i<5;i++){
+		// 	productor_threads[i] = std::thread(&BPlusTree::multi_insert, std::ref(bpt), rand()/10, rand()/10);
+		// }
+		// for(int i=5;i<10;i++){
+		// 	productor_threads[i] = std::thread(&BPlusTree::multi_remove, std::ref(bpt), rand()/10);
+		// }
+		// productor_threads[10] = std::thread(&BPlusTree::multi_search, std::ref(bpt), rand()/10);
+		// productor_threads[11] = std::thread(&BPlusTree::multi_update, std::ref(bpt), rand()/10, rand()/10);
+		// for(int i=0;i<12;i++){
+		// 	consumer_threads[i]=std::thread(&BPlusTree::consume_task, std::ref(bpt));
+		// }
+
+		// for(int i=0;i<11;i++){
+		// 	productor_threads[i].join();
+		// 	consumer_threads[i].join();
+		// }
+		int threads_half_num = rand()%(BUF_SIZE/4*3); // 随机生成生产者的数量
+		for(int i=0;i<threads_half_num;i++){ // 各个操作之间的比例   插入：删除：查找：更新 = 4:4:1:1
 			switch (rand()%10)
 			{
 			case 0:
 			case 1:
 			case 2:
 			case 3:
-				productor_threads[i] = std::thread(&BPlusTree::multi_insert, std::ref(bpt), rand(), rand());
+				productor_threads[i] = std::thread(&BPlusTree::multi_insert, std::ref(bpt), rand()/10, rand()/10);
 				break;
 			case 4:
 			case 5:
 			case 6:
 			case 7:
-				productor_threads[i] = std::thread(&BPlusTree::multi_remove, std::ref(bpt), rand());
+				productor_threads[i] = std::thread(&BPlusTree::multi_remove, std::ref(bpt), rand()/10);
 				break;
 			case 8:
-				productor_threads[i] = std::thread(&BPlusTree::multi_search, std::ref(bpt), rand());
+				productor_threads[i] = std::thread(&BPlusTree::multi_search, std::ref(bpt), rand()/10);
 				break;
 			case 9:
-				productor_threads[i] = std::thread(&BPlusTree::multi_update, std::ref(bpt), rand(), rand());
+				productor_threads[i] = std::thread(&BPlusTree::multi_update, std::ref(bpt), rand()/10, rand()/10);
 				break;
 			}
 		}
 
-		int consumer_num = rand()%(buf_size); // 随机生成消费者的数量，其大小在缓冲区大小以内
-		for(int i=0;i<consumer_num;i++){
+
+		for(int i=0;i<threads_half_num;i++){ // 创建消费者线程
 			consumer_threads[i] = std::thread(&BPlusTree::consume_task, std::ref(bpt));
 		}
 
 
-		for(int i=0;i<productor_num;i++){
+		for(int i=0;i<threads_half_num;i++){
 			productor_threads[i].detach();
-		}
-		for(int i=0;i<consumer_num;i++){
 			consumer_threads[i].detach();
 		}
 		// sleep(1);
-		usleep(100);
+		usleep(50);
 		
 	}
 	//bpt.print_tree();
